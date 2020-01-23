c9949079fd3d72d0608dd19688c2c58c64680493
Jeff Breig <jjbreig@gmail.com>
2020-01-22 23:41:15 -0500

    mvp

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,25 @@
+import {useState, useEffect} from 'react';
+import {useLocalStorage} from './useLocalStorage';
+
+const useDarkMode = () => {
+    const [isDarkMode, setDarkMode] = useState(false);
+    const [storedValue, setValue] = useLocalStorage("darkMode", 'false');
+
+    const flipBoolean = (value) => {
+        setDarkMode(value);
+        setValue(isDarkMode);
+    }
+
+    useEffect(() => {
+     if  (storedValue===true) {
+       document.body.className = 'dark-mode'
+     }
+     else{
+       document.body.className = ''
+     };
+
+    }, [storedValue]);
+    return [isDarkMode, flipBoolean]
+};
+
+export default useDarkMode;
