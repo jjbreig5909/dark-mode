22b5eab244d5d2bf0301a3a2b23109b4824b19ca
Jeff Breig <jjbreig@gmail.com>
2020-01-23 21:12:16 -0500

    added stretch

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -11,6 +11,12 @@ const Charts = ({ coinData }) => {
           <div className="coin__logo">
             <img src={coin.image} height="40" alt={coin.name} />
           </div>
+          <div className = "coin-information">
+            <p>All time high: <span className="coin-data">{Math.round(coin.ath)}</span></p>
+            <p>Market Cap: <span className="coin-data">{coin.market_cap}</span></p>
+            <p>Current Price: <span className="coin-data">{Math.round(coin.current_price)}</span></p>
+            <p>24 hour price change: <span className="coin-data">{Math.round(coin.price_change_24h)}</span></p>
+          </div>
           <Chart sparklineData={coin.sparkline_in_7d.price} />
         </div>
       ))}
